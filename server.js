9828c470db32d0e76fa7fc750d8ab8b3aaac4948
Nephi <nephi.calvin@gmail.com>
2017-10-28 18:46:04 -0400

    Mongo

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express(); 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 9000;
+var morgan = require('morgan');
+var mongoose = require('mongoose')
 
-app.get('/', function(req,res){
-	res.send('Hello World');
+app.use(morgan('dev'));
+
+mongoose.connect('localhost:27017/test', function(err){
+	if(err){
+		console.log('Not connected')
+	}else{
+		console.log('connected')
+	}
 });
+
+app.get('/home', function(req, res){
+	res.send('Hello from home');
+})
 
 app.listen(port, function(){
 	console.log('running made change ' + port);
