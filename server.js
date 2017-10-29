ac07d56be904e4f5e36e33c03a846f1886c1eb24
Nephi <nephi.calvin@gmail.com>
2017-10-28 20:23:10 -0400

    Routing, User model

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express(); 
 var port = process.env.PORT || 9000;
 var morgan = require('morgan');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router);
+var path = require('path');
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public')) //frontend
+app.use('/api',appRoutes) //Backend routes
 
-mongoose.connect('localhost:27017/test', function(err){
+
+mongoose.connect('mongodb://localhost:27017/test', function(err){
 	if(err){
 		console.log('Not connected')
 	}else{
@@ -14,10 +22,10 @@ mongoose.connect('localhost:27017/test', function(err){
 	}
 });
 
-app.get('/home', function(req, res){
-	res.send('Hello from home');
-})
+app.get('*',function(req, res){
+	res.sendFile(path.join(__dirname +'/public/app/views/index.html'));
+});
 
 app.listen(port, function(){
-	console.log('running made change ' + port);
+	console.log('running at ' + port);
 });
